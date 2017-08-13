aec194442a3ee50b467dd04b6652ff0237c794b6
Li <nothing@nothing.notemail>
2017-08-13 22:05:21 +1000

    added generation of tests.js for use in unittests. 	test.js will be generated from test.ts 	and it has access to all typescript code added placeholders for Phaser states

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,22 @@ module.exports = function(grunt) {
         // configure task provided by grunt-typescript plugin
         typescript: {
             base: {
-                src: ['src/ts/**/*.ts'], // which files to select
+                src: ['src/ts/game/main.ts'], // which files to select
                 dest: 'build/dev-dist/js/main.js', // where they'll go
                 options: {
                     module: 'amd', // module resolving mechanism
                     target: 'es5', // target version of EcmaScript
                     sourceMap: true // generate source maps
+                }
+            },
+
+            tests: {
+                src: ['src/ts/test.ts'], // which files to select
+                dest: 'build/dev-dist/js/tests.js', // where they will go,
+                options: {
+                    module: 'amd', // module resolving mechanism
+                    target: 'es5', // target version of EcmaScript
+                    sourceMap: true // generate sourcemaps
                 }
             }
         },
@@ -38,7 +48,8 @@ module.exports = function(grunt) {
                 bsFiles: {
                     // which files to host
                     src: [
-                        'build/dev-dist/**/*' 
+                        'build/dev-dist/**/*',
+                        'src/**/*' // for debugging with sourcemaps 
                     ]
                 },
                 options: {
@@ -54,7 +65,7 @@ module.exports = function(grunt) {
             dev: {
                 files: [
                     // copy src/html/dev-dist/index.html to build/dev-dist/index.html
-                    {expand: true, cwd: 'src/html/dev-dist', src: ['index.html'], dest: 'build/dev-dist/'},
+                    {expand: true, cwd: 'src/html/dev-dist', src: ['**/*.html'], dest: 'build/dev-dist/'},
 
                     // copy all files from vendor/js to build/dev/dist/js 
                     {expand: true, cwd: 'vendor/js', src: ['**/*'], dest: 'build/dev-dist/js/'},
@@ -69,7 +80,7 @@ module.exports = function(grunt) {
             dist: {
                 files: [
                     // copy src/html/dist/index.html to build/dist/index.html
-                    {expand: true, cwd: 'src/html/dist', src: ['index.html'], dest: 'build/dist/'},
+                    {expand: true, cwd: 'src/html/dist', src: ['**/*.html'], dest: 'build/dist/'},
 
                     // copy all files from assets/optimized to build/dist
                     {expand: true, cwd: 'assets/optimized', src: ['**/*'], dest: 'build/dist/'},
